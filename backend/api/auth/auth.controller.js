01d765a378915a3d4c3cb4dff474b5c827ba2fe7
Bercode257 <berc.david@gmail.com>
2022-01-13 14:01:01 +0200

    auth controller

---
+++ backend/api/auth/auth.controller.js
@@ -0,0 +1,43 @@
+const authService = require('')
+const logger = require('../../services/logger.service')
+
+async function login(req, res) {
+    const { username, password } = req.body
+    try {
+        const user = await authService.login(username, password)
+        req.session.user = user
+        res.json(user)
+    } catch (err) {
+        logger.error(`Failed to Login ${err}`)
+        res.status(401).send({ err: 'Failed to login' })
+    }
+}
+
+async function signup(req, res) {
+    const { username, password, fullname } = req.body
+    try {
+        const account = await authService.signup(username, password, fullname)
+        logger.debug(`auth.route - new account created: ${JSON.stringify(account)}`)
+        const user = await authService.login(username, password)
+        req.session.user = user
+        res.json(user)
+    } catch (err) {
+        logger.error(`Failed to signup ${err}`)
+        res.status(500).send({ err: 'Failed to signup' })
+    }
+}
+
+async function logout(req, res) {
+    try {
+        req.session.user = null
+        res.send({ msg: 'Logged out successfully' })
+    } catch (err) {
+        res.status(500).send({ err: 'Failed to logout' })
+    }
+}
+
+module.exports = {
+    login,
+    signup,
+    logout
+}
