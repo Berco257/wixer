ca4c556667c8d2b6b6c9489360c10c57e51a0df4
Bercode257 <berc.david@gmail.com>
2022-01-13 23:46:59 +0200

    remove getById func

--- backend/api/user/user.service.js
+++ backend/api/user/user.service.js
@@ -3,20 +3,6 @@ const fs = require('fs')
 const logger = require('../../services/logger.service')
 const gUsers = JSON.parse(require('../../data/users.json'))
 
-async function getById(userId) {
-    try {
-        let user = gUsers.find(user => user._id === userId)
-        if (user) {
-            user = { ...user }
-            delete user.password
-        }
-        return user
-    } catch (err) {
-        logger.error(`while finding user ${userId}`, err)
-        throw err
-    }
-}
-
 async function getByUsername(username) {
     try {
         let user = gUsers.find(user => user.username === username)
